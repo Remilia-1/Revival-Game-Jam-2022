c24626d230d5e0c901986af867279827b48e9312
Remilia1 <elotex1yt@gmail.com>
2022-10-11 19:13:33 +0200

    Character Model Rotation

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,14 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform forwardSource;
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform characterModel;
 
     [Header("Locomotion Settings")]
     [SerializeField] private float walkSpeed;
+
+    [Header("Character Model Settings")]
+    [SerializeField] private float rotationOffset = -180f;
+    Quaternion modelLookRotation;
 
     // Inputs
     MainInputProfile inputs;
@@ -30,11 +35,22 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Movement();
+        CharacterRotation();
     }
 
     private void Movement()
     {
         Vector3 moveDir = forwardSource.right * moveInput.x + forwardSource.forward * moveInput.y;
         characterController.Move(moveDir.normalized * walkSpeed * Time.deltaTime);
+    }
+
+    private void CharacterRotation()
+    {
+        Vector3 lookRotation = new Vector3(moveInput.x, 0, moveInput.y);
+
+        if(lookRotation.magnitude >= 0.08f)
+            modelLookRotation = Quaternion.LookRotation(lookRotation, Vector3.up) * Quaternion.AngleAxis(rotationOffset, Vector3.up);
+
+        characterModel.transform.rotation = modelLookRotation;
     }
 }
